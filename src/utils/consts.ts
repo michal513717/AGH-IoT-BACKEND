ecb6fe4a1dd2c1dce5c82696cd0fb591fcc2721a
Michal Kus <michal513717@gmail.com>
2025-10-14 21:27:54 +0200

    Added databse names

---
+++ src/utils/consts.ts
@@ -0,0 +1,6 @@
+export const DATABASE_NAMES = {
+  DIODE_COLLECTION: 'diode_status_collection',
+  LIGHT_INTENSITY_COLLECTION: 'light_intensity_collection',
+  TEMPERATURE_COLLECTION: 'temperature_collection',
+  WATER_LEVEL_COLLECTION: 'water_level_collection',
+};
